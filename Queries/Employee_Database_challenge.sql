a842ce09561c4bf2de81670406e03be17615dfb1
Angienoelhaverly <73972332+Angienoelhaverly@users.noreply.github.com>
2021-01-12 15:56:23 -0800

    Deliverable 2 updates

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,10 +1,12 @@
+-- Deliverable 1: The Number of Retiring Employees by Title
+-- Create new table for retirement titles
 SELECT e.emp_no, 
         e.first_name,
         e.last_name,
         ti.title,
         ti.from_date,
         ti.to_date
--- INTO retirement_titles
+INTO retirement_titles
 FROM employees as e
 INNER JOIN titles as ti
 ON (e.emp_no = ti.emp_no)
@@ -23,7 +25,27 @@ ORDER BY emp_no, to_date DESC;
 --retrieve the number of employees by their most recent job title 
 --who are about to retire.
 SELECT COUNT (emp_no), title
--- INTO retiring_titles
+INTO retiring_titles
 FROM unique_titles  
 GROUP BY title
 ORDER BY COUNT DESC; 
+
+-- Deliverable 2: The Employees Eligible for the Mentorship Program
+-- Mentorship Eligibility table that holds the employees who are 
+-- eligible to participate in a mentorship program.
+SELECT DISTINCT ON (e.emp_no) e.emp_no, 
+        e.first_name,
+        e.last_name, 
+        e.birth_date,
+        de.from_date,
+        de.to_date,
+        ti.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dep_employees as de
+ON e.emp_no = de.emp_no
+INNER JOIN titles as ti
+ON e.emp_no = ti.emp_no
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no; 
