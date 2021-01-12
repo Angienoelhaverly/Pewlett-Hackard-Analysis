3f1f5078989c3985cdfdc51cae6077087484ad23
Angienoelhaverly <73972332+Angienoelhaverly@users.noreply.github.com>
2021-01-11 18:55:38 -0800

    adding queries

--- schema.sql
+++ schema.sql
@@ -8,12 +8,13 @@ CREATE TABLE departments (
 DROP TABLE department;
 
 CREATE TABLE employees (
-	emp_no INT NOT NULL,
-	birth_date DATE NOT NULL, 
-	first_name VARCHAR NOT NULL, 
-	gender VARCHAR NOT NULL, 
-	hire_date DATE NOT NULL,
-	PRIMARY KEY (emp_no)
+     emp_no INT NOT NULL,
+     birth_date DATE NOT NULL,
+     first_name VARCHAR NOT NULL,
+     last_name VARCHAR NOT NULL,
+     gender VARCHAR NOT NULL,
+     hire_date DATE NOT NULL,
+     PRIMARY KEY (emp_no)
 ); 
 
 CREATE TABLE dep_manager(
@@ -26,14 +27,14 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (emp_no, dept_no)
 ); 
 
-CREATE TABLE salaries(
-	emp_no INT NOT NULL, 
-    dept_no VARCHAR(4) NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL, 
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no), 
-	PRIMARY KEY (emp_no)
-); 
+CREATE TABLE salaries (
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
 
 CREATE TABLE dep_employees(
 	emp_no INT NOT NULL, 
@@ -44,7 +45,7 @@ FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (emp_no, dept_no)
 ); 
-DROP TABLE titles;
+
 CREATE TABLE titles(
 	emp_no INT NOT NULL, 
     title VARCHAR(30) NOT NULL,
@@ -55,3 +56,8 @@ CREATE TABLE titles(
 ); 
 
 SELECT * FROM departments; 
+SELECT * FROM dep_employees;
+SELECT * FROM dep_manager;
+SELECT * FROM salaries;
+SELECT * FROM titles;
+
